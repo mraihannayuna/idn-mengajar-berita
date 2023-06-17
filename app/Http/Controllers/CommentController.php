9f68c55a28148ca11cf5a06724424acb20dee26d
mraihannayuna <m.raihannayua13@gmail.com>
2023-06-17 14:08:44 +0700

    nambah commentator

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -15,3 +15,4 @@ class CommentController extends Controller
     }
 
 }
+
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,12 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'commentator_id', 'post_id', 'comment', 'commentator_name'
+        'commentator_id', 'post_id', 'comment', 'commentator'
     ];
+
+    /**
+ * Get the user that owns the Comment
+ *
+ * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+ */
+public function commentator(): BelongsTo
+{
+    return $this->belongsTo(User::class, 'commentator_id', 'id');
 }
+
+}
+
+
+
